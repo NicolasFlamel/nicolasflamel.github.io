ef10fcc81ae9c70194e5f8efd1fac0ffa8f9acff
Amauri <hamauri@aol.com>
2023-02-10 17:19:31 -0800

    updated form to send emails

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,33 +1,42 @@
-import { useState } from 'react';
+import 'bootstrap/js/dist/alert';
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_KEY);
   const styles = {
     messageBox: {
       resize: 'none',
     },
   };
 
+  if (state.succeeded) {
+    return <div className="alert alert-info" role="alert">
+      Message was sent. Thank you.
+    </div>
+  }
+
   return (
-    <section className="container d-flex just" id="contact">
-      <form
-        className="col-sm-6"
-        action="https://formspree.io/f/mvonkbog"
-        method="POST"
-      >
+    <section className="container d-flex" id="contact">
+      <form onSubmit={handleSubmit} className="col-sm-6">
         <section>
-          <label for="name" className="form-label">
+          <label htmlFor="name" className="form-label">
             Name:
           </label>
-          <input name="name" id="name" className="form-control" />
+          <input name="name" id="name" className="form-control" required={true} />
         </section>
         <section>
-          <label for="email" className="form-label">
+          <label htmlFor="email" className="form-label">
             Email:
           </label>
-          <input name="email" id="email" className="form-control" />
+          <input name="email" id="email" className="form-control" required={true} />
+          <ValidationError
+            prefix="Email"
+            field="email"
+            errors={state.errors}
+          />
         </section>
         <section>
-          <label for="message" className="form-label">
+          <label htmlFor="message" className="form-label">
             Message:
           </label>
           <textarea
@@ -36,9 +45,19 @@ function Contact() {
             className="form-control"
             rows="10"
             style={styles.messageBox}
+            required={true}
+          />
+          <ValidationError
+            prefix="Message"
+            field="message"
+            errors={state.errors}
           />
         </section>
-        <button type="submit" className="btn text-light bg-secondary">
+        <button
+          type="submit"
+          className="btn text-light bg-secondary"
+          disabled={state.submitting}
+        >
           Send
         </button>
       </form>
