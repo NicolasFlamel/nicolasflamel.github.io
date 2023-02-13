368e51a4116399d47a380852b4b87a33c791547f
Amauri <hamauri@aol.com>
2023-02-13 12:39:04 -0800

    added resume file/link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,11 @@
+import resumePDF from '../../assets/resume.pdf'
+
 function Resume() {
-  return <section id="resume"></section>;
+
+  return <section id="resume">
+    <h2>Resume</h2>
+    <p>Download my <a href={resumePDF} download="Amauri's Resume">resume</a></p>
+  </section>;
 }
 
 export default Resume;
