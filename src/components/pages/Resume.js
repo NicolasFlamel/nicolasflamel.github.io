01e15d07d747587bf9296f64392e1e22dc13dbbc
Amauri <hamauri@aol.com>
2023-02-13 12:43:18 -0800

    background/prettier

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,11 +1,17 @@
-import resumePDF from '../../assets/resume.pdf'
+import resumePDF from '../../assets/resume.pdf';
 
 function Resume() {
-
-  return <section id="resume">
-    <h2>Resume</h2>
-    <p>Download my <a href={resumePDF} download="Amauri's Resume">resume</a></p>
-  </section>;
+  return (
+    <section className="bg-gray p-5" id="resume">
+      <h2>Resume</h2>
+      <p>
+        Download my{' '}
+        <a href={resumePDF} download="Amauri's Resume">
+          resume
+        </a>
+      </p>
+    </section>
+  );
 }
 
 export default Resume;
