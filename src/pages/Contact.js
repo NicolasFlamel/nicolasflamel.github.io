15368d30b6ce3869a702b28aa565620d7dd2f7ff
Amauri <hamauri@aol.com>
2023-02-14 12:36:57 -0800

    refactored contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,17 +2,62 @@ import 'bootstrap/js/dist/alert';
 import { useState } from 'react';
 import { useForm, ValidationError } from '@formspree/react';
 
-function Contact() {
-  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_KEY);
-  const [isNameEmpty, setIsNameEmpty] = useState();
-  const [isEmailEmpty, setIsEmailEmpty] = useState();
-  const [isMessageEmpty, setIsMessageEmpty] = useState();
+function FormFields(props) {
+  const [isFieldEmpty, setIsFieldEmpty] = useState();
 
-  const styles = {
-    messageBox: {
-      resize: 'none',
-    },
+  const label = () => {
+    return (
+      <input
+        name={props.name}
+        id={props.name}
+        className="form-control"
+        onBlur={(e) =>
+          e.target.value.length > 0
+            ? setIsFieldEmpty(false)
+            : setIsFieldEmpty(true)
+        }
+      />
+    );
   };
+
+  const textArea = () => {
+    return (
+      <textarea
+        name={props.name}
+        id={props.name}
+        className="form-control"
+        rows="10"
+        style={{ resize: 'none' }}
+        onBlur={(e) =>
+          e.target.value.length > 0
+            ? setIsFieldEmpty(false)
+            : setIsFieldEmpty(true)
+        }
+      />
+    );
+  };
+
+  return (
+    <section className="m-2">
+      <label htmlFor={props.name} className="form-label">
+        {props.name + ':'}
+      </label>
+      {props.name === 'message' ? textArea() : label()}
+      {isFieldEmpty ? <p>This field cannot be empty</p> : null}
+      {props.name === 'email' ? (
+        <ValidationError
+          prefix="This"
+          field="email"
+          errors={props.state.errors}
+        />
+      ) : null}
+    </section>
+  );
+}
+
+function Contact() {
+  const fields = ['name', 'email', 'message'];
+  const [state, handleSubmit] = useForm(process.env.REACT_APP_FORM_KEY);
 
   if (state.succeeded) {
     return (
@@ -26,57 +71,9 @@ function Contact() {
     // add WHEN I move my cursor out of one of the form fields without entering text
     <section className="container d-flex bg-gray p-5" id="contact">
       <form onSubmit={handleSubmit} className="col-sm-6">
-        <section className="m-2">
-          <label htmlFor="name" className="form-label">
-            Name:
-          </label>
-          <input
-            name="name"
-            id="name"
-            className="form-control"
-            onBlur={(e) =>
-              e.target.value.length > 0
-                ? setIsNameEmpty(false)
-                : setIsNameEmpty(true)
-            }
-          />
-          {isNameEmpty ? <p>This field cannot be empty</p> : null}
-        </section>
-        <section className="m-2">
-          <label htmlFor="email" className="form-label">
-            Email:
-          </label>
-          <input
-            name="email"
-            id="email"
-            className="form-control"
-            onBlur={(e) =>
-              e.target.value.length > 0
-                ? setIsEmailEmpty(false)
-                : setIsEmailEmpty(true)
-            }
-          />
-          {isEmailEmpty ? <p>This field cannot be empty</p> : null}
-          <ValidationError prefix="This" field="email" errors={state.errors} />
-        </section>
-        <section className="m-2">
-          <label htmlFor="message" className="form-label">
-            Message:
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="form-control"
-            rows="10"
-            style={styles.messageBox}
-            onBlur={(e) =>
-              e.target.value.length > 0
-                ? setIsMessageEmpty(false)
-                : setIsMessageEmpty(true)
-            }
-          />
-          {isMessageEmpty ? <p>This field cannot be empty</p> : null}
-        </section>
+        {fields.map((field) => (
+          <FormFields key={field} name={field} state={state} />
+        ))}
         <button
           type="submit"
           className="btn text-light bg-secondary m-2"
