fc51cdbb743eebb8a5077a1ca95c90f576727fad
Amauri <hamauri@aol.com>
2023-11-06 14:52:55 -0800

    made slideshow transition smoother

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -1,18 +1,21 @@
-import { useState } from 'react';
+import { createRef, useState } from 'react';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import projectList from '../assets/projectList';
 
 const topThreeProjects = projectList.slice(0, 3);
 
 function Slideshow() {
+  const nodeRef = createRef(null);
   const [slide, setSlide] = useState(0);
+  const [disableButton, setDisableButton] = useState(false);
 
   const slideButtonHandler = (e) => {
     const buttonValue = e.target.className === 'next' ? 1 : -1;
+    const newSlide = slide + buttonValue;
 
-    if (slide + buttonValue > topThreeProjects.length - 1) setSlide(0);
-    else if (slide + buttonValue < 0) setSlide(topThreeProjects.length - 1);
-    else setSlide(slide + buttonValue);
+    if (newSlide > topThreeProjects.length - 1) setSlide(0);
+    else if (newSlide < 0) setSlide(topThreeProjects.length - 1);
+    else setSlide(newSlide);
   };
 
   return (
@@ -28,11 +31,15 @@ function Slideshow() {
               key={topThreeProjects[slide].title}
               timeout={1000}
               classNames="fade"
+              nodeRef={nodeRef}
+              onEntering={() => setDisableButton(true)}
+              onEntered={() => setDisableButton(false)}
             >
               <img
                 src={topThreeProjects[slide].img.src}
                 alt={topThreeProjects[slide].img.alt}
                 style={{ width: '100%' }}
+                ref={nodeRef}
               />
             </CSSTransition>
           </TransitionGroup>
@@ -41,7 +48,11 @@ function Slideshow() {
       <button className="prev" onClick={slideButtonHandler}>
         &#10094;
       </button>
-      <button className="next" onClick={slideButtonHandler}>
+      <button
+        className="next"
+        onClick={slideButtonHandler}
+        disabled={disableButton}
+      >
         &#10095;
       </button>
     </div>
