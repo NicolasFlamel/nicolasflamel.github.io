947962b9306960be4530683f94ca139c5bb0e1ba
Amauri <hamauri@aol.com>
2023-12-15 11:55:40 -0800

    fixed eslint errors

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,10 @@
-import { useState } from 'react';
 import socials from '../assets/socialList';
 
 // returns anchor element with a separate isHover state for each
 const List = ({ social }) => {
-  const [isHover, setIsHover] = useState(false);
-
-  const handleEvent = ({ type }) =>
-    type === 'mouseenter' ? setIsHover(true) : setIsHover(false);
-
   return (
-    <a
-      href={social.href}
-      target="_blank"
-      title={social.title}
-      rel="noreferrer"
-      onMouseEnter={handleEvent}
-      onMouseLeave={handleEvent}
-    >
-      <img {...social.img} />
+    <a href={social.href} target="_blank" title={social.title} rel="noreferrer">
+      <img src={social.img.src} alt={social.img.alt} />
     </a>
   );
 };
