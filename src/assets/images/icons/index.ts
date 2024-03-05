702c29efb5625f64ddd80e6fd3023b87e02cc2b1
Amauri <hamauri@aol.com>
2024-03-05 12:29:46 -0800

    reworked resume page
    updated resume page to instead just being a bullet list if skills they are now icons of the skills themselves
    
    added multiple svg files to fill in for proficiencies

---
+++ src/assets/images/icons/index.ts
@@ -0,0 +1,39 @@
+import apiIconObj from './api-icon.svg';
+import apolloGraphqlIconObj from './apollo-graphql-icon.svg';
+import bootstrapIconObj from './bootstrap-icon.svg';
+import cssIconObj from './css-icon.svg';
+import expressJSObj from './expressjs-icon.svg';
+import handlebarsIconObj from './handlebars-icon.svg';
+import htmlIconObj from './html-icon.svg';
+import javaScriptObj from './javascript-icon.svg';
+import jQueryObj from './jquery-icon.svg';
+import localStorageIconObj from './localStorage-icon.svg';
+import mongoDBIconObj from './mongodb-icon.svg';
+import nodeJSObj from './nodejs-icon.svg';
+import pwaIconObj from './pwa-icon.svg';
+import reactIconObj from './react-icon.svg';
+import responsiveObj from './responsive-icon.svg';
+import sequelizeIconObj from './sequelize-icon.svg';
+import typescriptObj from './typescript-icon.svg';
+
+export {
+  apiIconObj,
+  apolloGraphqlIconObj,
+  bootstrapIconObj,
+  cssIconObj,
+  expressJSObj,
+  handlebarsIconObj,
+  htmlIconObj,
+  javaScriptObj,
+  jQueryObj,
+  localStorageIconObj,
+  mongoDBIconObj,
+  nodeJSObj,
+  pwaIconObj,
+  reactIconObj,
+  responsiveObj,
+  sequelizeIconObj,
+  typescriptObj,
+};
+
+declare module '*.svg';
--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,31 +1,11 @@
+import {
+  frontendProficiencies,
+  backendProficiencies,
+} from '../assets/proficienciesList';
+
 function Resume() {
   const resumeLink =
     'https://drive.google.com/file/d/1ny84bhM9ylXxOtOHLhSOuXJaOA99_V4s/view?usp=sharing';
-  const styles = {
-    listStyle: {
-      listStyleType: 'circle',
-    },
-  };
-
-  const frontendProficiencies = [
-    'HTML/CSS',
-    'JavaScript',
-    'React',
-    'TypeScript',
-    'jQuery',
-    'Bootstrap',
-    'Handlebars',
-    'Responsive Design',
-    'Local Storage, Session Storage, IndexedDB',
-  ];
-  const backendProficiencies = [
-    'APIs',
-    'REST',
-    'Node.js, Express',
-    'MySQL, Sequelize',
-    'MongoDB, Mongoose',
-    'Apollo GraphQL',
-  ];
 
   return (
     <section id="resume">
@@ -39,9 +19,14 @@ function Resume() {
       <article>
         <h3>Front-end Proficiencies</h3>
         <ul>
-          {frontendProficiencies.map((string) => (
-            <li style={styles.listStyle} key={string}>
-              {string}
+          {frontendProficiencies.map((proficiency) => (
+            <li key={proficiency.skill}>
+              <img
+                className="proficiency-icon"
+                src={proficiency.icon.src}
+                alt={proficiency.icon.alt}
+              />
+              <p>{proficiency.skill}</p>
             </li>
           ))}
         </ul>
@@ -49,9 +34,14 @@ function Resume() {
       <article>
         <h3>Back-end Proficiencies</h3>
         <ul>
-          {backendProficiencies.map((string) => (
-            <li style={styles.listStyle} key={string}>
-              {string}
+          {backendProficiencies.map((proficiency) => (
+            <li key={proficiency.skill}>
+              <img
+                className="proficiency-icon"
+                src={proficiency.icon.src}
+                alt={proficiency.icon.alt}
+              />
+              <p>{proficiency.skill}</p>
             </li>
           ))}
         </ul>
