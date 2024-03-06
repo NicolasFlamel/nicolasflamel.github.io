0e41024e7b208e0e15a726cc12741ebe4974dffc
Amauri <hamauri@aol.com>
2024-03-05 16:24:46 -0800

    updated about me page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
       <section style={styles.title}>
         <h1>About me</h1>
       </section>
-      <section>
+      <section className="intro">
         <img
           src={portrait.src}
           alt={portrait.alt}
@@ -34,25 +34,11 @@ const About = () => {
           I'm just starting off in. Please, help yourself and look through my
           portfolio and feel free to contact me if you wish.
         </p>
-        <section style={{ display: 'flex' }}>
-          <h3>Backstory</h3>
-          <i>If you're interested</i>
-        </section>
+        <h3>What I'm up to</h3>
         <p>
-          Back in highschool a friend of mine in this field taught very small
-          amount of coding and loved it but unfortunately due to circumstances
-          out of my control I couldn't finish college for a degree in computer
-          science. So went on to work at a hotel for 7 years. Then after having
-          enough of working a job I had no passion for I decided I had enough so
-          I moved to california,{' '}
-          <i>for other reasons that are not important to the story</i>, and
-          started planning to change. Through some luck a friend told me about a
-          coding boot camp given by UC Davis so I did some research and it
-          seemed like a perfect fit for where I was and wanted. After a few
-          months that brings us to the now where I am almost done with the boot
-          camp and am looking for potential employers. If you have an interest
-          in talking about hiring me feel free to send me a message through the
-          contact me page.
+          I'm currently working for a non-profit organization as their only
+          full-stack web developer creating and updating different web
+          applications for them.
         </p>
       </section>
       <Slideshow />
